d2e83465ee784a8c08eeb92a550a261bf639e29e
kakaluote444 <liuburu@outlook.com>
2017-05-14 11:04:14 +0800

    论文第一次格式修改

---
+++ yooai-service-admin/src/main/java/com/jxufe/bhind/control/AdminPageControl.java
@@ -0,0 +1,7 @@
+package com.jxufe.bhind.control;
+
+/**
+ * Created by liuburu on 2017/5/9.
+ */
+public class AdminPageControl {
+}
---
+++ yooai-service-web/src/main/java/com/jxufe/front/control/LoginRegisterControl.java
@@ -0,0 +1,7 @@
+package com.jxufe.front.control;
+
+/**
+ * Created by liuburu on 2017/5/10.
+ */
+public class LoginRegisterControl {
+}
